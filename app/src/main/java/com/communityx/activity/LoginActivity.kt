210ba037965aa710c62fac6807b9275b3a63ba53
shubhamkotnala <shubhamkotnala664@gmail.com>
2019-04-26 13:06:41 +0530

    first commit

---
+++ app/src/main/java/com/communityx/activity/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.communityx.activity
+
+import android.content.Intent
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import com.communityx.R
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
