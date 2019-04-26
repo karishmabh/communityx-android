d85302230a60eb3e6164eeadfd9b3fed8badd8eb
shubhamkotnala <shubhamkotnala664@gmail.com>
2019-04-26 17:50:19 +0530

    Refactor: improved UI

--- app/src/main/java/com/communityx/activity/SignUpStudentInfoActivity.kt
+++ app/src/main/java/com/communityx/activity/SignUpStudentInfoActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
 import com.communityx.R
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 import kotlinx.android.synthetic.main.layout_top_view_logo.*
 
 class SignUpStudentInfoActivity : AppCompatActivity() {
@@ -14,5 +15,6 @@ class SignUpStudentInfoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up_student_info)
 
         text_subtitle.text = "Build your social impact identity on CommunityX."
+
     }
 }
