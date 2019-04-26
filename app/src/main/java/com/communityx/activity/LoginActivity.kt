aaff0f8d7e6f7d16dd301dfa53bf61e9079cb9ec
shubhamkotnala <shubhamkotnala664@gmail.com>
2019-04-26 16:49:01 +0530

    Feature: SignupCategoryActivity: Added UI

--- app/src/main/java/com/communityx/activity/LoginActivity.kt
+++ app/src/main/java/com/communityx/activity/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.communityx.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
@@ -13,5 +14,10 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         edit_password.transformationMethod = PasswordTransformationMethod.getInstance()
+
+        text_signup.setOnClickListener{
+            startActivity(Intent(this, SignupCategoryActivity::class.java))
+            overridePendingTransition(R.anim.anim_next_slide_in, R.anim.anim_next_slide_out)
+        }
     }
 }
