4610d9e85d64ef76d87e5a0c8ba514b7d572751c
shubhamkotnala <shubhamkotnala664@gmail.com>
2019-04-26 14:59:57 +0530

    Feature: WelcomeActivity: added UI

---
+++ app/src/main/java/com/communityx/activity/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.communityx.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.communityx.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
