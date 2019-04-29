bbb8f81b4570114672a02ab36e66e7e0de878900
Ajay Vyas <ajv_007@hotmail.com>
2019-04-29 16:25:53 +0530

    Feature:added image chooser dialog for sign up

--- app/src/main/java/com/communityx/activity/SignUpStudentInfoActivity.java
+++ app/src/main/java/com/communityx/activity/SignUpStudentInfoActivity.java
@@ -1,16 +1,16 @@
 package com.communityx.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.communityx.R;
 import com.communityx.R.id;
-import java.util.HashMap;
-import kotlin.Metadata;
-import kotlin.jvm.internal.Intrinsics;
 import org.jetbrains.annotations.Nullable;
 
 public class SignUpStudentInfoActivity extends AppCompatActivity {
@@ -18,11 +18,31 @@ public class SignUpStudentInfoActivity extends AppCompatActivity {
     @BindView(id.text_subtitle)
     TextView textSubtitle;
 
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_student_info);
         ButterKnife.bind(this);
 
         textSubtitle.setText("Build your social impact identity on CommunityX.");
+
+    }
+
+    @OnClick(id.card_add_image)
+    void chooseImage(){
+       showImageChooserDialog();
+    }
+
+
+
+    private void showImageChooserDialog(){
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_image_chooser,null);
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(dialogView);
+        bottomSheetDialog.show();
+
+        View layoutCamera = dialogView.findViewById(id.layout_camera);
+        View layoutGallery = dialogView.findViewById(id.layout_gallery);
+
     }
 }
