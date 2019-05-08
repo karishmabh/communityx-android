60a0df1fe4e9d51d8ac661a7fd3c173ef0353404
Abhishek <abhishekkanojia493@gmail.com>
2019-05-08 19:26:26 +0530

    Feature: PaymentActivity: Added new ui

---
+++ app/src/main/java/com/communityx/utils/AppConstant.java
@@ -0,0 +1,8 @@
+package com.communityx.utils;
+
+public interface AppConstant {
+
+    String SAVECARD = "saved_card";
+    String DEBITCARD = "debit_card";
+    String CREDITCARD = "credit_card";
+}
