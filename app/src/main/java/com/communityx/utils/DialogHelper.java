6acb5602935fb1351dd97e0e0c2efcd21629e617
Syed Hussain Mehdi <shmehdi01@gmail.com>
2019-05-10 12:48:19 +0530

    Refactor:DialogHelper:improved code

--- app/src/main/java/com/communityx/utils/DialogHelper.java
+++ app/src/main/java/com/communityx/utils/DialogHelper.java
@@ -2,7 +2,7 @@ package com.communityx.utils;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -11,15 +11,19 @@ import com.communityx.R;
 
 public class DialogHelper {
 
-    public static void showEventDialog(Context context, String content){
+    public static void showReportingDialog(Context context,@NonNull String content){
         Dialog dialog = new Dialog(context);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         Window window = dialog.getWindow();
         window.setLayout(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-        //window.setBackgroundDrawableResource(android.R.color.transparent);
-        window.setContentView(R.layout.dialog_create_event);
+        window.setContentView(R.layout.dialog_reporting);
         dialog.setCancelable(true);
         dialog.show();
+
+        ImageView imageClose = dialog.findViewById(R.id.image_cross);
+        imageClose.setOnClickListener(v -> dialog.dismiss());
+        TextView textContent = dialog.findViewById(R.id.text_content);
+        textContent.setText(content);
     }
 
     public static void showDonationSuccessDialog(Context context, String amount){
