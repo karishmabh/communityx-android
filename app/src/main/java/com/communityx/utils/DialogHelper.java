d80819a691e4edee4a22fe8b568255020f287b89
Syed Hussain Mehdi <shmehdi01@gmail.com>
2019-05-10 12:33:39 +0530

    Feature:dialog_donation_success: layout added

--- app/src/main/java/com/communityx/utils/DialogHelper.java
+++ app/src/main/java/com/communityx/utils/DialogHelper.java
@@ -2,8 +2,11 @@ package com.communityx.utils;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.communityx.R;
 
 public class DialogHelper {
@@ -17,5 +20,21 @@ public class DialogHelper {
         window.setContentView(R.layout.dialog_create_event);
         dialog.setCancelable(true);
         dialog.show();
+    }
+
+    public static void showDonationSuccessDialog(Context context, String amount){
+        Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window window = dialog.getWindow();
+        assert window != null;
+        window.setLayout(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        window.setContentView(R.layout.dialog_donation_success);
+        dialog.setCancelable(true);
+        dialog.show();
+
+        ImageView imageClose = dialog.findViewById(R.id.image_cross);
+        imageClose.setOnClickListener(v -> dialog.dismiss());
+        TextView textDonation = dialog.findViewById(R.id.text_amount);
+        textDonation.setText(amount);
     }
 }
