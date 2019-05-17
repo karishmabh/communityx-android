cce52e604a3d1f02818cf7ad8f3046c34953e48b
JitenderAndroid <this.jitu@gmail.com>
2019-05-17 18:51:17 +0530

    feature: MessageActivity: modified chat screen ui

--- app/src/main/java/com/communityx/activity/MessageActivity.java
+++ app/src/main/java/com/communityx/activity/MessageActivity.java
@@ -1,9 +1,14 @@
 package com.communityx.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -16,6 +21,11 @@ public class MessageActivity extends AppCompatActivity {
 
     @BindView(R.id.recycler_chat)
     RecyclerView recyclerViewChat;
+    @BindView(R.id.edit_chat)
+    EditText editChat;
+
+    private ArrayList<String> mChatList;
+    private ChatAdapter mChatAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +34,60 @@ public class MessageActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setRecyclerViewChat();
+        setEditChatLocationClick();
+    }
+
+    private void setEditChatLocationClick() {
+        editChat.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (editChat.getRight() - editChat.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        startActivityForResult(new Intent(MessageActivity.this, SendLocationActivity.class), 101);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            try {
+                String filename = data.getExtras().getString("bitmap");
+                mChatList.add(filename);
+                mChatAdapter.notifyDataSetChanged();
+                recyclerViewChat.scrollToPosition(mChatList.size() - 1);
+            } catch (Exception e) {
+
+            }
+        }
     }
 
     @OnClick(R.id.imageBack)
     void backTapped() {
-       super.onBackPressed();
+        super.onBackPressed();
     }
 
     private void setRecyclerViewChat() {
+        mChatList = new ArrayList<>();
+        mChatList.add("S");
+        mChatList.add("S");
+        mChatList.add("S");
+        mChatList.add("S");
+        mChatList.add("d");
+        mChatList.add("R");
+        mChatList.add("R");
+        mChatList.add("R");
+        mChatList.add("R");
+
         recyclerViewChat.setLayoutManager(new LinearLayoutManager(this));
-        recyclerViewChat.setAdapter(new ChatAdapter(MessageActivity.this, new ArrayList<String>()));
+        mChatAdapter = new ChatAdapter(MessageActivity.this, mChatList);
+        recyclerViewChat.setAdapter(mChatAdapter);
     }
 }
--- app/src/main/java/com/communityx/adapters/ChatAdapter.java
+++ app/src/main/java/com/communityx/adapters/ChatAdapter.java
@@ -6,8 +6,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.communityx.R;
+import com.communityx.utils.Utils;
 
 import java.util.ArrayList;
 
@@ -16,6 +19,7 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final int VIEW_TYPE_DATE = 0;
     private final int VIEW_TYPE_SENDER = 1;
     private final int VIEW_TYPE_RECEIVER = 2;
+    private final int VIEW_TYPE_MAP = 3;
 
     private Activity activity;
     private ArrayList<String> mChatList;
@@ -36,28 +40,36 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         } else if (viewType == VIEW_TYPE_RECEIVER) {
             View view = LayoutInflater.from(activity).inflate(R.layout.item_receiver, parent, false);
             return new ChatAdapter.ReceiverViewHolder(view);
+        } else if (viewType == VIEW_TYPE_MAP) {
+            View view = LayoutInflater.from(activity).inflate(R.layout.item_map_added, parent, false);
+            return new ChatAdapter.MapViewHolder(view);
         }
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        if (viewHolder instanceof MapViewHolder) {
+            ((MapViewHolder) viewHolder).bindData();
+        }
     }
 
     @Override
     public int getItemViewType(int position) {
-        if (position < 5) {
+        if (mChatList.get(position).equalsIgnoreCase("r")) {
             return VIEW_TYPE_RECEIVER;
-        } else if (position == 5) {
+        } else if (mChatList.get(position).equalsIgnoreCase("d")) {
             return VIEW_TYPE_DATE;
-        } else {
+        } else if (mChatList.get(position).equalsIgnoreCase("s")) {
             return VIEW_TYPE_SENDER;
+        } else {
+            return VIEW_TYPE_MAP;
         }
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return mChatList.size();
     }
 
     class ReceiverViewHolder extends RecyclerView.ViewHolder {
@@ -81,6 +93,21 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         public DateViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+        }
+    }
+
+    class MapViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.image_map)
+        ImageView imageMap;
+
+        public MapViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindData() {
+            imageMap.setImageBitmap(Utils.convertToBitmap(activity, mChatList.get(getAdapterPosition())));
         }
     }
 }
