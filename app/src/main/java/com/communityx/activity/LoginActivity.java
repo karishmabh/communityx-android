7b2d83763952567193346ab08f04814e6d912b29
JitenderAndroid <this.jitu@gmail.com>
2019-05-21 18:53:49 +0530

    refactor: improved login activity code style

--- app/src/main/java/com/communityx/activity/LoginActivity.java
+++ app/src/main/java/com/communityx/activity/LoginActivity.java
@@ -1,21 +1,23 @@
 package com.communityx.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.PasswordTransformationMethod;
 import android.widget.Button;
+
+import com.communityx.R;
+import com.communityx.R.id;
+import com.communityx.utils.Utils;
+
+import org.jetbrains.annotations.Nullable;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnTextChanged;
-import com.communityx.R;
-import com.communityx.R.id;
-import com.communityx.utils.Utils;
 import kotlin.jvm.internal.Intrinsics;
-import org.jetbrains.annotations.Nullable;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,30 +33,30 @@ public class LoginActivity extends AppCompatActivity {
         this.setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-        Utils.enableButton(buttonLogin,false);
+        Utils.enableButton(buttonLogin, false);
         Intrinsics.checkExpressionValueIsNotNull(editPassword, "edit_password");
         editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
     }
 
     @OnClick(id.text_signup)
-    void goToSignUp(){
+    void goToSignUp() {
         LoginActivity.this.startActivity(new Intent(LoginActivity.this, SignupCategoryActivity.class));
         LoginActivity.this.overridePendingTransition(R.anim.anim_next_slide_in, R.anim.anim_next_slide_out);
     }
 
     @OnClick(id.button_login)
-    void loginClicked(){
+    void loginClicked() {
         LoginActivity.this.startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
         LoginActivity.this.overridePendingTransition(R.anim.anim_next_slide_in, R.anim.anim_next_slide_out);
     }
 
     @OnTextChanged(id.edit_email_username)
-    void onEmailTyping(CharSequence s){
+    void onEmailTyping(CharSequence s) {
         Utils.enableButton(buttonLogin, s.length() != 0 && editPassword.length() != 0);
     }
 
     @OnTextChanged(id.edit_password)
-    void onPasswordTyping(CharSequence s){
+    void onPasswordTyping(CharSequence s) {
         Utils.enableButton(buttonLogin, s.length() != 0 && editEmail.length() != 0);
     }
 }
