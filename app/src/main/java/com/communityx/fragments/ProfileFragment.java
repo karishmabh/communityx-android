8a5acc94bdce42dde91af8c947fb4593e5ddbe56
JitenderAndroid <this.jitu@gmail.com>
2019-05-30 19:30:31 +0530

    refactor: added profile screen

--- app/src/main/java/com/communityx/fragments/ProfileFragment.java
+++ app/src/main/java/com/communityx/fragments/ProfileFragment.java
@@ -1,7 +1,6 @@
 package com.communityx.fragments;
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -10,29 +9,49 @@ import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.communityx.R;
 import com.communityx.activity.EventActivity;
+import com.communityx.activity.LoginActivity;
 import com.communityx.activity.ProfileActivity;
+import com.communityx.models.login.LoginResponse;
+import com.communityx.models.logout.LogoutResponse;
+import com.communityx.network.DataManager;
+import com.communityx.network.ResponseListener;
+import com.communityx.utils.AppConstant;
+import com.communityx.utils.AppPreference;
 import com.communityx.utils.CustomToolBarHelper;
-import de.hdodenhof.circleimageview.CircleImageView;
+import com.communityx.utils.DialogHelper;
+import com.communityx.utils.Utils;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public class ProfileFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileFragment extends Fragment implements AppConstant {
 
     @BindView(R.id.image_user_profile)
     CircleImageView imageUser;
+    @BindView(R.id.linear_top)
+    LinearLayout linearTop;
+    @BindView(R.id.text_name)
+    TextView textName;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         ButterKnife.bind(this,view);
         CustomToolBarHelper customToolBarHelper = new CustomToolBarHelper(view);
         customToolBarHelper.setTitle(R.string.my_profile);
+        textName.setText(AppPreference.Companion.getInstance(getActivity()).getString(PREF_EMAIL));
         return view;
     }
 
@@ -52,5 +71,38 @@ public class ProfileFragment extends Fragment {
             startActivity(new Intent(getContext(), EventActivity.class));
             Objects.requireNonNull(getActivity()).overridePendingTransition(R.anim.anim_next_slide_in,R.anim.anim_next_slide_out);
         }
+    }
+
+    @OnClick(R.id.constraint_logout)
+    void tappedLogout() {
+        DialogHelper.showLogoutDialog(getActivity(), new DialogHelper.IDialogCallback() {
+            @Override
+            public void onConfirmed() {
+                doLogout();
+            }
+
+            @Override
+            public void onDenied() {
+            }
+        });
+    }
+
+    private void doLogout() {
+        if (getActivity() == null) return;
+
+        DataManager.INSTANCE.doLogOut(getActivity(), new ResponseListener<LogoutResponse>() {
+            @Override
+            public void onSuccess(LogoutResponse response) {
+
+                AppPreference.Companion.getInstance(getActivity()).clearSession();
+                startActivity(new Intent(getActivity(), LoginActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+                Toast.makeText(getActivity(), "You have been Logout successfully!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(@NotNull Object error) {
+                Utils.showError(getActivity(), linearTop, error);
+            }
+        });
     }
 }
--- app/src/main/java/com/communityx/models/login/Profile.kt
+++ app/src/main/java/com/communityx/models/login/Profile.kt
@@ -2,6 +2,9 @@ package com.communityx.models.login
 
 data class Profile(
     val admin: List<Admin>,
+    val student: List<Student>,
+    val professional: List<Professional>,
+    val organisation: List<Organisation>,
     val category: String,
     val email: String,
     val id: String,
