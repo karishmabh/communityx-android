5843a9360d2a758911500db2572124b9c889f2d5
JitenderAndroid <this.jitu@gmail.com>
2019-06-03 16:51:05 +0530

    feature: inflate real time data of connect allies

--- app/src/main/java/com/communityx/adapters/CommunityAlliesAdapter.kt
+++ app/src/main/java/com/communityx/adapters/CommunityAlliesAdapter.kt
@@ -2,6 +2,7 @@ package com.communityx.adapters
 
 import android.app.Activity
 import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,13 +10,16 @@ import android.widget.CheckBox
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.communityx.R
+import com.communityx.models.connect_allies.Interest
+import com.communityx.models.connect_allies.ProfileData
+import com.communityx.utils.AppConstant
+import com.communityx.utils.AppConstant.*
 import com.google.android.flexbox.FlexboxLayout
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_connect_allies.view.*
 
-import java.util.ArrayList
-import java.util.Arrays
-
-class CommunityAlliesAdapter(private val mArrayList: ArrayList<String>, private val mActvity: Activity) :
-    RecyclerView.Adapter<CommunityAlliesAdapter.EventHolder>() {
+class CommunityAlliesAdapter(private val mArrayList: List<ProfileData>, private val mActvity: Activity) : AppConstant,
+        RecyclerView.Adapter<CommunityAlliesAdapter.EventHolder>() {
 
     private val mLayoutInflater: LayoutInflater = LayoutInflater.from(mActvity)
 
@@ -25,33 +29,65 @@ class CommunityAlliesAdapter(private val mArrayList: ArrayList<String>, private 
     }
 
     override fun onBindViewHolder(eventHolder: EventHolder, i: Int) {
-        val list = Arrays.asList(*arrayOf("School Safety", "Immigration", "LGBTQ+", "Mental health", "Prisom Reform"))
-        setFLexLayout(eventHolder.flexboxLayout, list)
+        eventHolder.bindData()
+        val interesttList = mArrayList.get(i).profile.interests
+        setFLexLayout(eventHolder.flexboxLayout, interesttList)
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return mArrayList.size
     }
 
-    class EventHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class EventHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         @BindView(R.id.flex_layout_allies)
         lateinit var flexboxLayout: FlexboxLayout
 
         init {
             ButterKnife.bind(this, itemView)
         }
+
+        fun bindData() {
+            val profileData = mArrayList.get(adapterPosition);
+            if (!TextUtils.isEmpty(profileData.profile.profile_image)) {
+                Picasso.get().load(profileData.profile.profile_image).error(R.drawable.profile_placeholder).fit().into(itemView.circle_profile_image)
+            }
+
+            //itemView.text_title_name.setText(profileData.profile.full_name)
+            itemView.text_description.setText(setSubTitle(profileData.category))
+        }
+
+        fun setSubTitle(category: String) : String {
+            when (category) {
+                ACTION_SIGN_UP_STUDENT -> {
+                    itemView.text_title_name.setText(mArrayList.get(adapterPosition).profile.full_name)
+                    return mArrayList.get(adapterPosition).profile.standard + ","+ mArrayList.get(adapterPosition).profile.standard_name
+                }
+
+                ACTION_SIGN_UP_ORGANIZATION ->  {
+                    itemView.text_title_name.setText(mArrayList.get(adapterPosition).profile.name)
+                    return "Organization" + "," + mArrayList.get(adapterPosition).profile.name
+                }
+
+                ACTION_SIGN_UP_PROFESSIONAL ->  {
+                    itemView.text_title_name.setText(mArrayList.get(adapterPosition).profile.full_name)
+                    return mArrayList.get(adapterPosition).profile.job_title + ","+mArrayList.get(adapterPosition).profile.company_name
+                }
+            }
+            return ""
+        }
     }
 
-    fun setFLexLayout(fLexLayout: FlexboxLayout?, interest: List<String>) {
+
+    fun setFLexLayout(fLexLayout: FlexboxLayout?, interest: List<Interest>) {
         fLexLayout!!.removeAllViews()
-        for (civilRight in interest) {
+        for (i in interest.indices) {
             val checkBox = LayoutInflater.from(mActvity).inflate(R.layout.item_interest, null) as CheckBox
-            checkBox.text = civilRight
+            checkBox.text = interest.get(i).name
             checkBox.performClick()
             checkBox.setOnCheckedChangeListener { buttonView, isChecked -> checkBox.setBackgroundResource(if (isChecked) R.drawable.bg_interest_active else R.drawable.bg_interest_inactive) }
 
             val lp =
-                ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             lp.setMargins(10, 10, 10, 10)
             fLexLayout.addView(checkBox, lp)
         }
