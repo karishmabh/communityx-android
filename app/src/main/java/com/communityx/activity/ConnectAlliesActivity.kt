a8afcaebc23f6030f1e1df6999e7d4db6c007384
JitenderAndroid <this.jitu@gmail.com>
2019-06-03 16:50:19 +0530

    feature: ConnectAlliesActivity: added Connect allies api

--- app/src/main/java/com/communityx/activity/ConnectAlliesActivity.kt
+++ app/src/main/java/com/communityx/activity/ConnectAlliesActivity.kt
@@ -8,77 +8,54 @@ import butterknife.ButterKnife
 import butterknife.OnClick
 import com.communityx.R
 import com.communityx.adapters.CommunityAlliesAdapter
-import com.communityx.models.login.Data
-import com.communityx.models.login.LoginRequest
-import com.communityx.models.login.LoginResponse
+import com.communityx.models.connect_allies.ProfileData
 import com.communityx.network.DataManager
 import com.communityx.network.ResponseListener
-import com.communityx.utils.AppConstant
-import com.communityx.utils.AppPreference
 import com.communityx.utils.CustomProgressBar
 import com.communityx.utils.Utils
 import kotlinx.android.synthetic.main.activity_connect_allies.*
 import kotlinx.android.synthetic.main.layout_top_view.*
-import java.util.*
 
 class ConnectAlliesActivity : AppCompatActivity() {
-    private val alliesList = ArrayList<String>()
     private var communityAlliesAdapter: CommunityAlliesAdapter? = null
-    private lateinit var phoneNumber: String
-    private lateinit var password: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_allies)
         ButterKnife.bind(this)
 
-        text_title!!.text = getString(R.string.connect_with_allias)
-        text_description!!.text = getString(R.string.we_found_global)
+        text_title.text = getString(R.string.connect_with_allias)
+        text_description.text = getString(R.string.we_found_global)
 
-        getIntentData()
-        setAdapter(alliesList)
-    }
-
-    private fun getIntentData() {
-        phoneNumber = intent.getStringExtra(AppConstant.PHONE_KEY)
-        password = intent.getStringExtra(AppConstant.PASSWORD_KEY)
+        getSuggestionsList()
     }
 
     @OnClick(R.id.button_community)
     internal fun buttonCommunityTapped() {
-        performLogin()
+        navigateActivity()
     }
 
-    private fun performLogin() {
-        val dialog = CustomProgressBar.getInstance(this).showProgressDialog("Logging in...")
-        DataManager.doLogin(this, LoginRequest(phoneNumber, password), object : ResponseListener<LoginResponse> {
-            override fun onSuccess(response: LoginResponse) {
-
+    private fun getSuggestionsList() {
+        val dialog = CustomProgressBar.getInstance(this).showProgressDialog("loading...")
+        DataManager.getConnectingAllies(this, object : ResponseListener<List<ProfileData>> {
+            override fun onSuccess(response: List<ProfileData>) {
                 dialog.dismiss()
-                val loginData = response.data.get(0)
-                saveUserData(loginData)
+
+                val alliesList = response
+                setAdapter(alliesList)
             }
 
             override fun onError(error: Any) {
                 dialog.dismiss()
                 Utils.showError(this@ConnectAlliesActivity, constraint_layout, error)
             }
-
         })
     }
 
-    fun setAdapter(alliesList: ArrayList<String>) {
-        recycler_view!!.layoutManager = LinearLayoutManager(this)
+    fun setAdapter(alliesList: List<ProfileData>) {
+        recycler_view.layoutManager = LinearLayoutManager(this)
         communityAlliesAdapter = CommunityAlliesAdapter(alliesList, this)
-        recycler_view!!.adapter = communityAlliesAdapter
-    }
-
-    private fun saveUserData(loginData: Data) {
-        AppPreference.getInstance(this).setString(AppConstant.PREF_SESSION_ID, loginData.session.session_id)
-        // AppPreference.getInstance(this).setString(AppConstant.PREF_EMAIL, loginData.profile.email)
-        AppPreference.getInstance(this).setBoolean(AppConstant.PREF_IS_LOGIN, true)
-
-        navigateActivity()
+        recycler_view.adapter = communityAlliesAdapter
     }
 
     private fun navigateActivity() {
