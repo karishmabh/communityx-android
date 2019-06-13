acd5d94e2c749bcb43ed1f17828bb19c24226d08
JitenderAndroid <this.jitu@gmail.com>
2019-06-13 19:33:44 +0530

    refactor: resolved bug of app closed on moving from profile

--- app/src/main/java/com/communityx/activity/DashboardActivity.kt
+++ app/src/main/java/com/communityx/activity/DashboardActivity.kt
@@ -1,23 +1,15 @@
 package com.communityx.activity
 
 import android.os.Bundle
-import android.os.Handler
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
-import android.view.animation.RotateAnimation
-import android.widget.ImageView
-import butterknife.BindString
-import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import com.communityx.R
 import com.communityx.fragments.*
 import com.communityx.utils.AnimationUtils
-import com.communityx.utils.DialogHelper
 import com.communityx.utils.Utils
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
@@ -39,22 +31,12 @@ class DashboardActivity : AppCompatActivity(), BottomNavigationView.OnNavigation
         when (menuItem.itemId) {
             R.id.navigation_friends -> replceFragment(MyAllFriendsFragment(), "fragment_myfriends")
             R.id.navigation_community -> {
-                if (isBackPressClick) {
-                    isBackPressClick = false
-                    hasGoneToProfileViewImage = false
-                    return true
-                } else if (hasGoneToProfileViewImage) {
-                    onBackPressed()
-                    return true
-                }
                 AnimationUtils.rotateView(image_feed!!, 0, 800)
                 replceFragment(CommunityFeedFragment(), "fragment_community")
             }
             R.id.navigation_message -> replceFragment(MessageFragment(), "fragment_message")
             R.id.navigation_profile -> {
-                if (hasGoneToProfileViewImage) {
-                    return true
-                }
+                hasGoneToProfileViewImage = true
                 replceFragment(ProfileFragment(), "fragment_profile")
             }
             R.id.navigation_notifications -> replceFragment(NotificationFragment(), "fragment_notification")
@@ -73,9 +55,10 @@ class DashboardActivity : AppCompatActivity(), BottomNavigationView.OnNavigation
 
     override fun onBackPressed() {
         if (hasGoneToProfileViewImage) {
-            isBackPressClick = true
             nav_view!!.selectedItemId = R.id.navigation_community
+            hasGoneToProfileViewImage = false
+        } else {
+            super.onBackPressed()
         }
-        super.onBackPressed()
     }
 }
