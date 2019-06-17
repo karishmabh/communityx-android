e3059de8e56225255df365cf215954c30c8876a0
JitenderAndroid <this.jitu@gmail.com>
2019-06-17 19:14:49 +0530

    refcator: ForgotPasswordActivity: added forgot password screen

---
+++ app/src/main/java/com/communityx/activity/ForgotPasswordActivity.kt
@@ -0,0 +1,16 @@
+package com.communityx.activity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import com.communityx.R
+
+import kotlinx.android.synthetic.main.activity_forgot_password.*
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
