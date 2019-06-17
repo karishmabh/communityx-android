f4956e8bb0fe53594a1593f0336aaee7ae2dcb43
JitenderAndroid <this.jitu@gmail.com>
2019-06-17 19:16:37 +0530

    refcator: SignupOrganisation: added verify user api to check for user exist before

--- app/src/main/java/com/communityx/fragments/SignUpOrganizationFragment.kt
+++ app/src/main/java/com/communityx/fragments/SignUpOrganizationFragment.kt
@@ -14,9 +14,13 @@ import android.view.ViewGroup
 import android.widget.EditText
 import butterknife.ButterKnife
 import butterknife.OnClick
+import butterknife.OnTextChanged
 import com.communityx.R
 import com.communityx.base.BaseSignUpFragment
-import com.communityx.models.signup.*
+import com.communityx.models.signup.EmailPhoneVerificationRequest
+import com.communityx.models.signup.OtpRequest
+import com.communityx.models.signup.SignUpRequest
+import com.communityx.models.signup.VerifyOtpRequest
 import com.communityx.network.DataManager
 import com.communityx.network.ResponseListener
 import com.communityx.network.serviceRepo.SignUpRepo
@@ -25,6 +29,7 @@ import com.communityx.utils.CustomProgressBar
 import com.communityx.utils.GalleryPicker
 import com.communityx.utils.SnackBarFactory
 import com.communityx.utils.Utils
+import com.mukesh.OnOtpCompletionListener
 import kotlinx.android.synthetic.main.fragment_sign_up_organization.*
 
 class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPickerListener {
@@ -34,6 +39,8 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
     private var shouldChangeNumber = false
     private var isDelKeyPressed = false
     private var prefix: String? = null
+    val selectionStart: Int = 3
+    private var otpMain: String? = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_sign_up_organization, container, false)
@@ -51,9 +58,16 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
         super.onActivityCreated(savedInstanceState)
         edit_mobile.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus && edit_mobile.text.toString() == prefix) {
-                edit_mobile.setSelection(2)
+                edit_mobile.setSelection(selectionStart)
             }
         }
+
+        view_otp.setOtpCompletionListener(object: OnOtpCompletionListener {
+            override fun onOtpCompleted(otp: String?) {
+                otpMain = otp
+                createOtpAndVerify()
+            }
+        })
 
         edit_mobile.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
@@ -65,14 +79,29 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(!s.toString().startsWith("+1")){
+                if(!s.toString().startsWith(resources.getString(R.string.prefix_number))){
                     edit_mobile.setText(prefix)
                     Selection.setSelection(edit_mobile.getText(), edit_mobile.text!!.length)
                 }
             }
         })
 
+        edit_postalcode.onFocusChangeListener = object: View.OnFocusChangeListener {
+            override fun onFocusChange(v: View?, hasFocus: Boolean) {
+                if (!hasFocus && edit_postalcode.text.toString().length < 5) {
+                    textinput_postalcode.setError(resources.getString(R.string.postal_code_should_be_six_character))
+                }
+            }
+        }
+
         view_password.visibility = if (signUpActivity?.isOtpVerified!!) View.VISIBLE else View.GONE
+    }
+
+    @OnTextChanged(R.id.edit_postalcode)
+    fun onPostalCodeChanged() {
+        if (edit_postalcode.text.toString().length == 5) {
+            textinput_postalcode.setError(null)
+        }
     }
 
     override fun setFieldsData(): Boolean {
@@ -81,7 +110,7 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
         signUpActivity?.signUpRequest?.website = edit_website.text.toString()
         signUpActivity?.signUpRequest?.postal_code = edit_postalcode.text.toString()
         if (edit_mobile.text.toString().length > 4)
-            signUpStudent?.phone = edit_mobile.text.toString().substring(2)
+            signUpStudent?.phone = edit_mobile.text.toString().substring(selectionStart)
         signUpStudent?.password = edit_confirm_password.text.toString()
 
         return validateEmpty(signUpActivity?.signUpRequest)
@@ -116,7 +145,7 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
                 if (showSnackbar) edit_website.requestFocus()
             }
 
-            !Utils.isValid(edit_website.text.toString()) -> {
+            !Utils.validateWebsite(edit_website.text.toString()) -> {
                 isValidate = false
                 errorMessage = getString(R.string.string_website_invalid)
                 if (showSnackbar) edit_website.requestFocus()
@@ -136,7 +165,7 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
                 isValidate = false
                 errorMessage = getString(R.string.mobile_field_empty)
                 edit_mobile.requestFocus()
-                edit_mobile.setSelection(2)
+                edit_mobile.setSelection(selectionStart)
             }
             !hasOtpOrPasswordFieldVisible -> {
                 isValidate = false
@@ -173,7 +202,27 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
     }
 
     override fun onContinueButtonClicked() {
-        if (setFieldsData()) proceedToNextPage()
+        if (setFieldsData()) {
+            context?.let {
+                val dialog = CustomProgressBar.getInstance(it).showProgressDialog("verifying data ...")
+                dialog.show()
+                    val emailPhoneVerificationRequest = EmailPhoneVerificationRequest(signUpActivity?.signUpRequest?.phone.toString().trim(), signUpActivity?.signUpRequest?.email.toString().trim())
+
+                    activity?.let {
+                        DataManager.doVerifyEmailPhone(it, emailPhoneVerificationRequest, object : ResponseListener<List<String>> {
+                            override fun onSuccess(response: List<String>) {
+                                dialog.dismiss()
+                                proceedToNextPage()
+                            }
+
+                            override fun onError(error: Any) {
+                                dialog.dismiss()
+                                Utils.showError(activity, constraint_root, error)
+                            }
+                        })
+                    }
+            }
+        }
     }
 
     override fun onMediaSelected(imagePath: String, uri: Uri?, isImage: Boolean) {
@@ -207,7 +256,28 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
         } else {
             edit_mobile.isEnabled = true
             shouldChangeNumber = false
-            text_send_otp.text = getString(R.string.send_otp)
+            onMobileChanged()
+        }
+    }
+
+    fun onMobileChanged() {
+        text_send_otp.text = getString(R.string.send_otp)
+
+        text_enter_otp.visibility = View.GONE
+        view_otp.visibility = View.GONE
+        resend_otp.visibility = View.GONE
+        view_password.visibility = View.GONE
+
+        edit_create_password.setText("")
+        edit_confirm_password.setText("")
+    }
+
+    @OnTextChanged(R.id.edit_confirm_password)
+    fun confirmPasswordChanged() {
+        if (edit_confirm_password.text.toString().length > 0) {
+            enableButton(true)
+        } else {
+            enableButton(false)
         }
     }
 
@@ -218,7 +288,7 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
             SnackBarFactory.createSnackBar(context, constraint_root, getString(R.string.mobile_field_empty))
             return
         }
-        val number = edit_mobile.text.toString().substring(2)
+        val number = edit_mobile.text.toString().substring(selectionStart)
         if (number.length < 10) {
             SnackBarFactory.createSnackBar(context, constraint_root, getString(R.string.mobiel_number_less_digits))
             return
@@ -255,12 +325,6 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
 
     private fun initOtpBox() {
         visibleOtpField(false)
-        initOtpTextWatcher(edit_otp_one, edit_otp_two, null)
-        initOtpTextWatcher(edit_otp_two, edit_otp_three, edit_otp_one)
-        initOtpTextWatcher(edit_otp_three, edit_otp_four, edit_otp_two)
-        initOtpTextWatcher(edit_otp_four, edit_otp_five, edit_otp_three)
-        initOtpTextWatcher(edit_otp_five, edit_otp_six, edit_otp_four)
-        initOtpTextWatcher(edit_otp_six, null, edit_otp_five)
     }
 
     private fun visibleOtpField(visible: Boolean) {
@@ -279,45 +343,17 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
         }
     }
 
-    private fun initOtpTextWatcher(currentEditText: EditText, nextEditText: EditText?, prevEditText: EditText?) {
-        currentEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                if (nextEditText != null && s.length == 1)
-                    nextEditText.requestFocus()
-                else if (prevEditText != null && s.isEmpty()) prevEditText.requestFocus()
-            }
-
-            override fun afterTextChanged(s: Editable) {
-                if (currentEditText == edit_otp_six && !TextUtils.isEmpty(edit_otp_six.text.toString())) {
-                    createOtpAndVerify()
-                }
-            }
-        })
-    }
 
     private fun createOtpAndVerify() {
-        val verifyOtpRequest =
-            VerifyOtpRequest(otp = getOtp(), phone = edit_mobile.text.toString().substring(2))
+        view_otp.setText("")
+        val verifyOtpRequest = VerifyOtpRequest(otp = otpMain!!, phone = edit_mobile.text.toString().substring(selectionStart))
         Utils.hideSoftKeyboard(activity)
         verifyOtp(verifyOtpRequest)
         hasOtpOrPasswordFieldVisible = true
     }
 
-    private fun getOtp(): String {
-        var strBuilder = StringBuilder()
-        strBuilder.append(edit_otp_one.text.toString())
-        strBuilder.append(edit_otp_two.text.toString())
-        strBuilder.append(edit_otp_three.text.toString())
-        strBuilder.append(edit_otp_four.text.toString())
-        strBuilder.append(edit_otp_five.text.toString())
-        strBuilder.append(edit_otp_six.text.toString())
-
-        return strBuilder.toString()
-    }
-
     private fun proceedToNextPage() {
+        changeButtonStatus(0, true)
         signUpActivity?.goToNextPage()
     }
 
@@ -328,17 +364,17 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
                 SnackBarFactory.createSnackBar(context, constraint_root, response)
                 view_password.visibility = View.VISIBLE
                 constraint_root.post { constraint_root.scrollTo(0, constraint_root.height) }
+
                 visibleOtpField(false)
                 signUpActivity?.isOtpVerified = true
                 edit_create_password.requestFocus()
-                clearOtp()
+                view_otp.setText("")
                 dialog.dismiss()
                 disabledMobileField(true)
             }
 
             override fun onError(error: Any) {
-                clearOtp()
-                edit_otp_one.requestFocus()
+                view_otp.setText("")
                 Utils.showError(activity, constraint_root, error)
                 signUpActivity?.isOtpVerified = false
                 dialog.dismiss()
@@ -346,18 +382,8 @@ class SignUpOrganizationFragment : BaseSignUpFragment(), GalleryPicker.GalleryPi
         })
     }
 
-    private fun clearOtp() {
-        edit_otp_one.setText("")
-        edit_otp_two.setText("")
-        edit_otp_three.setText("")
-        edit_otp_four.setText("")
-        edit_otp_five.setText("")
-        edit_otp_six.setText("")
-    }
-
     private fun disabledMobileField(disable: Boolean) {
         edit_mobile.isEnabled = !disable
         text_send_otp.text = if (!disable) getString(R.string.send_otp) else getString(R.string.change)
     }
-
 }
