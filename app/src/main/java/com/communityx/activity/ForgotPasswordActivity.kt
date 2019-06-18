4251c65eb671e9d703577b9703f32fe7dad366f9
JitenderAndroid <this.jitu@gmail.com>
2019-06-18 19:09:28 +0530

    refactor: ForgotPasswordActivity: improved forgot password ui

--- app/src/main/java/com/communityx/activity/ForgotPasswordActivity.kt
+++ app/src/main/java/com/communityx/activity/ForgotPasswordActivity.kt
@@ -3,14 +3,18 @@ package com.communityx.activity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.view.View
 import com.communityx.R
 
 import kotlinx.android.synthetic.main.activity_forgot_password.*
+import kotlinx.android.synthetic.main.layout_top_view_logo.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        text_subtitle.visibility = View.INVISIBLE
     }
 }
