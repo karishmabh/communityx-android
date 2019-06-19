de230cda88dd367d6efa1fa4a388648efd6cd387
JitenderAndroid <this.jitu@gmail.com>
2019-06-19 18:41:29 +0530

    refactor: customize toolbar for search

--- app/src/main/java/com/communityx/utils/CustomToolBarHelper.java
+++ app/src/main/java/com/communityx/utils/CustomToolBarHelper.java
@@ -1,11 +1,15 @@
 package com.communityx.utils;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
