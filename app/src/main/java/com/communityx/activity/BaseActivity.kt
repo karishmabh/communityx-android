13f250edf8cc113aa8f3b9f36af4d86b04c5abd0
JitenderAndroid <this.jitu@gmail.com>
2019-06-20 19:04:25 +0530

    feature: added required resource icons and minor ui changes

--- app/src/main/java/com/communityx/activity/BaseActivity.kt
+++ app/src/main/java/com/communityx/activity/BaseActivity.kt
@@ -1,8 +1,11 @@
 package com.communityx.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.communityx.R
 import com.communityx.utils.CustomToolBarHelper
@@ -34,5 +37,26 @@ open class BaseActivity : AppCompatActivity() {
 
         textTitle.text = title
         textSubtitle.text = subtitle
+    }
+
+    fun setToolbarReset(activity: Activity?, title: String, visible: Boolean) {
+        if (activity == null) return
+        val textTitle = activity.findViewById<TextView>(R.id.text_title)
+        val imageBack = activity.findViewById<ImageView>(R.id.image_back)
+        val linearLayout = activity.findViewById<LinearLayout>(R.id.view_toolbar)
+
+        imageBack.setOnClickListener { v ->
+            activity.onBackPressed()
+            overridePendingTransition(R.anim.anim_prev_slide_in, R.anim.anim_prev_slide_out)
+        }
+        textTitle.setText(title)
+
+        if(!visible)  linearLayout.visibility = View.INVISIBLE
+    }
+
+     fun startNewActivity(activity: AppCompatActivity, givenclas: Class<*>) {
+        val intent = Intent(activity, givenclas)
+        startActivity(intent)
+        overridePendingTransition(R.anim.anim_next_slide_in, R.anim.anim_next_slide_out)
     }
 }
