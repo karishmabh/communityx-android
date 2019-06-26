7277c30f5cb3f69645c3dabe75591d30aef1cfac
JitenderAndroid <this.jitu@gmail.com>
2019-06-26 10:57:39 +0530

    refactor: removed ui bugs from interest screen

--- app/src/main/java/com/communityx/adapters/SelectedInterestAdapter.kt
+++ app/src/main/java/com/communityx/adapters/SelectedInterestAdapter.kt
@@ -57,6 +57,7 @@ class SelectedInterestAdapter(val mInterestList: List<MinorsData>, val mActvity:
                 for (selectedid in mSelectedIds) {
                     if (civilRight.id == selectedid) {
                         checkBox.setBackgroundResource(R.drawable.bg_interest_active)
+                        checkBox.setTextColor(mActvity.resources.getColor(R.color.colorBlackTitle))
                     }
                 }
 
@@ -67,9 +68,11 @@ class SelectedInterestAdapter(val mInterestList: List<MinorsData>, val mActvity:
                             return@setOnCheckedChangeListener
                         }
                         checkBox.setBackgroundResource(com.communityx.R.drawable.bg_interest_active)
+                        checkBox.setTextColor(mActvity.resources.getColor(R.color.colorBlackTitle))
                         mSelectedIds.add(civilRight.id)
                     } else {
                         checkBox.setBackgroundResource(com.communityx.R.drawable.bg_interest_inactive)
+                        checkBox.setTextColor(mActvity.resources.getColor(R.color.colorLightGrey))
                         mSelectedIds.remove(civilRight.id)
                     }
 
