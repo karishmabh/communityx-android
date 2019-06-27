2c3e2c0380f518d962926ce0ae8a66ae4cdf1595
JitenderAndroid <this.jitu@gmail.com>
2019-06-27 17:38:11 +0530

    feature: added suggestion fragment apis

--- app/src/main/java/com/communityx/fragments/SuggestionFragment.kt
+++ app/src/main/java/com/communityx/fragments/SuggestionFragment.kt
@@ -1,45 +1,58 @@
 package com.communityx.fragments
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import butterknife.BindView
 import butterknife.ButterKnife
 import com.communityx.R
-import com.communityx.adapters.InvitationAdapter
 import com.communityx.adapters.SuggestionAdapter
-
-import java.util.ArrayList
+import com.communityx.models.myallies.invitation.AlliesInvitationResponse
+import com.communityx.models.myallies.invitation.DataX
+import com.communityx.network.DataManager
+import com.communityx.network.ResponseListener
+import com.communityx.utils.Utils
+import kotlinx.android.synthetic.main.fragment_suggestion.*
 
 class SuggestionFragment : Fragment() {
-    private val suggestionList = ArrayList<String>()
     private var suggestionAdapter: SuggestionAdapter? = null
 
-    @BindView(R.id.recycler_suggestion_list)
-    internal var recyclerSuggestionList: RecyclerView? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_suggestion, container, false)
         ButterKnife.bind(this, view)
-        setAdapter(suggestionList)
 
         return view
     }
 
-    fun setAdapter(mSuggestionList: ArrayList<String>) {
-        recyclerSuggestionList!!.layoutManager = LinearLayoutManager(activity)
-        suggestionAdapter = SuggestionAdapter(mSuggestionList, activity!!)
-        recyclerSuggestionList!!.adapter = suggestionAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getAllSuggestionsList()
+    }
+
+    private fun getAllSuggestionsList() {
+        progress_bar?.visibility = View.VISIBLE
+        DataManager.getAlliesSuggestions(activity!!, object : ResponseListener<AlliesInvitationResponse> {
+            override fun onSuccess(response: AlliesInvitationResponse) {
+                progress_bar?.visibility = View.GONE
+                var data = response.data
+                var userData = data.get(0).data
+
+                (parentFragment as MyAllFriendsFragment)?.updateTabText(2, userData.size)
+                setAdapter(userData)
+            }
+
+            override fun onError(error: Any) {
+                progress_bar?.visibility = View.GONE
+                Utils.showError(activity, frame_root, error)
+            }
+        })
+    }
+
+    fun setAdapter(dataX: List<DataX>) {
+        recycler_suggestion_list?.layoutManager = LinearLayoutManager(activity)
+        suggestionAdapter = SuggestionAdapter(dataX, activity!!)
+        recycler_suggestion_list?.adapter = suggestionAdapter
     }
 }
