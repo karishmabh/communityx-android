11b7bef2d10fe53ce9d7d0b87284d1f741766496
amit-opensource <amitgupta0294@gmail.com>
2019-06-28 19:12:11 +0530

    feature: ProfileActivity: implemented api

--- app/src/main/java/com/communityx/network/AppConfiguration.kt
+++ app/src/main/java/com/communityx/network/AppConfiguration.kt
@@ -3,6 +3,6 @@ package com.communityx.network
 interface AppConfiguration {
     companion object {
         val BASE_URL = "http://dev.communityx.crownstack.website/v1/"
-        val MOCK_URL = "https://394e8712-2544-4c59-8788-f03ca515dbf3.mock.pstmn.io/"
+        val MOCK_URL = "https://40f8abef-dcee-4fb5-940c-abd26df388bb.mock.pstmn.io/"
     }
 }
