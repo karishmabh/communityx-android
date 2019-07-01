4e3b672469b547e964cbebb58f37bde4f1a9c70c
JitenderAndroid <this.jitu@gmail.com>
2019-07-01 19:01:01 +0530

    feature: added apis for invitation update status

--- app/src/main/java/com/communityx/fragments/InvitationFragment.kt
+++ app/src/main/java/com/communityx/fragments/InvitationFragment.kt
@@ -6,16 +6,18 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import butterknife.ButterKnife
 import com.communityx.R
 import com.communityx.adapters.InvitationAdapter
-import com.communityx.models.myallies.invitation.AlliesInvitationResponse
-import com.communityx.models.myallies.invitation.DataX
+import com.communityx.models.myallies.all_allies.AllAlliesResponse
+import com.communityx.models.myallies.all_allies.DataX
+import com.communityx.models.myallies.all_allies.UpdateInvitationRequest
+import com.communityx.models.signup.SignUpResponse
 import com.communityx.network.DataManager
 import com.communityx.network.ResponseListener
 import com.communityx.utils.Utils
 import kotlinx.android.synthetic.main.fragment_invitation.*
-import java.util.*
 
 class InvitationFragment : Fragment() {
     private var invitationAdapter: InvitationAdapter? = null
@@ -34,14 +36,28 @@ class InvitationFragment : Fragment() {
 
     private fun getAllInvitaionsList() {
         progress_bar?.visibility = View.VISIBLE
-        DataManager.getAlliesInvitation(activity!!, object: ResponseListener<AlliesInvitationResponse> {
-            override fun onSuccess(response: AlliesInvitationResponse) {
+        DataManager.getAlliesInvitation(activity!!, object: ResponseListener<AllAlliesResponse> {
+            override fun onSuccess(response: AllAlliesResponse) {
                 progress_bar?.visibility = View.GONE
                 var data = response.data
                 var userData = data.get(0).data
 
                 (parentFragment as MyAllFriendsFragment)?.updateTabText(1, userData.size)
                 setAdapter(userData)
+            }
+
+            override fun onError(error: Any) {
+                progress_bar?.visibility = View.GONE
+                Utils.showError(activity, frame_root, error)
+            }
+        })
+    }
+
+    private fun updateInvitation(updateInvitationRequest: UpdateInvitationRequest) {
+        progress_bar?.visibility = View.VISIBLE
+        DataManager.updateInvitation(activity!!, updateInvitationRequest, object : ResponseListener<SignUpResponse> {
+            override fun onSuccess(response: SignUpResponse) {
+                progress_bar?.visibility = View.GONE
             }
 
             override fun onError(error: Any) {
