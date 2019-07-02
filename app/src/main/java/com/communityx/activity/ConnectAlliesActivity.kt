9c3ad4e1910db07c4be2da4a8f575554dcb54b14
JitenderAndroid <this.jitu@gmail.com>
2019-07-02 19:38:49 +0530

    feature: added send invitation api on connect allies screen

--- app/src/main/java/com/communityx/activity/ConnectAlliesActivity.kt
+++ app/src/main/java/com/communityx/activity/ConnectAlliesActivity.kt
@@ -5,20 +5,25 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import butterknife.ButterKnife
 import butterknife.OnClick
 import com.communityx.R
 import com.communityx.adapters.CommunityAlliesAdapter
 import com.communityx.models.connect_allies.ProfileData
+import com.communityx.models.signup.SignUpResponse
 import com.communityx.network.DataManager
 import com.communityx.network.ResponseListener
+import com.communityx.utils.AppConstant.ACCEPTED
 import com.communityx.utils.CustomProgressBar
 import com.communityx.utils.Utils
 import kotlinx.android.synthetic.main.activity_connect_allies.*
 import kotlinx.android.synthetic.main.layout_top_view.*
 
-class ConnectAlliesActivity : AppCompatActivity() {
+class ConnectAlliesActivity : AppCompatActivity(), CommunityAlliesAdapter.IOnAddFriendClicked {
     private var communityAlliesAdapter: CommunityAlliesAdapter? = null
+    private var listProfileData: ArrayList<ProfileData> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,8 +48,9 @@ class ConnectAlliesActivity : AppCompatActivity() {
                 dialog.dismiss()
 
                 val alliesList = response
+                listProfileData = alliesList as ArrayList<ProfileData>
                 if (alliesList.size > 0) {
-                    setAdapter(alliesList)
+                    setAdapter(listProfileData)
                 } else {
                     text_record_not_found.visibility = View.VISIBLE
                     recycler_view.visibility = View.GONE
@@ -60,7 +66,7 @@ class ConnectAlliesActivity : AppCompatActivity() {
 
     fun setAdapter(alliesList: List<ProfileData>) {
         recycler_view.layoutManager = LinearLayoutManager(this)
-        communityAlliesAdapter = CommunityAlliesAdapter(alliesList, this)
+        communityAlliesAdapter = CommunityAlliesAdapter(alliesList, this, this)
         recycler_view.adapter = communityAlliesAdapter
     }
 
@@ -68,5 +74,28 @@ class ConnectAlliesActivity : AppCompatActivity() {
         startActivity(Intent(this, DashboardActivity::class.java))
         overridePendingTransition(R.anim.anim_next_slide_in, R.anim.anim_next_slide_out)
         finish()
+    }
+
+    private fun sendInvitation(id: String, position: Int, imageView: ImageView) {
+        var dialog = CustomProgressBar.getInstance(this).showProgressDialog("sending invitation..")
+        dialog.show()
+
+        DataManager.sendInvitation(this, id, object : ResponseListener<SignUpResponse> {
+            override fun onSuccess(response: SignUpResponse) {
+                dialog.dismiss()
+
+                imageView.setImageResource(R.drawable.ic_suggestion_friend_sent_request)
+                Toast.makeText(this@ConnectAlliesActivity, "Request Sent successfully!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onError(error: Any) {
+                dialog.dismiss()
+                Utils.showError(this@ConnectAlliesActivity, constraint_layout, error)
+            }
+        })
+    }
+
+    override fun sendInvitationTapped(id: String, position: Int, imageView: ImageView) {
+        sendInvitation(id, position, imageView)
     }
 }
