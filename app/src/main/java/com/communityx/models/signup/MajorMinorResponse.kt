4b98d99c54ec5fadf692520e68e562ec955651d2
JitenderAndroid <this.jitu@gmail.com>
2019-07-03 19:34:10 +0530

    refactor: resolved interest apis stucture

--- app/src/main/java/com/communityx/models/signup/MajorMinorResponse.kt
+++ app/src/main/java/com/communityx/models/signup/MajorMinorResponse.kt
@@ -3,7 +3,7 @@ package com.communityx.models.signup
 import com.communityx.models.Error
 
 data class MajorMinorResponse(
-    val data: List<List<MinorsData>>,
+    val data: List<MinorsData>,
     val error: Error,
     val status: String
 )
